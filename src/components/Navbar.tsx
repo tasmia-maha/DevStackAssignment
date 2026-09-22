f3f70d601a924a027b98a26a4631f04e9d42a4b5
tasmia-maha <tasmiamaha2k5@gmail.com>
2026-09-23 00:27:19 +0600

    Fix navigation elements

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,7 +5,7 @@ function Navbar(){
     return (
         <nav className="sticky top-0 z-50 bg-white border-b">
             <div className="max-w-7xl flex justify-between items-center mx-auto px-4 py-5">
-                {/* Menu button */}
+                {/* Hamburger-mobile */}
                 <button type="button" aria-label="Toggle navigation menu" onClick={()=>setMenuOpen(!menuOpen)} className="md:hidden text-gray-500"><FiMenu size={25}/></button>
                 {/* logo */}
                 <div className="flex items-center gap-2">
@@ -15,9 +15,17 @@ function Navbar(){
                         <span/>
                         </span>
                 </div>
-                {/* menu */}
+                {/* Desktop menu */}
+                <div className="hidden items-center gap-5 text-sm font-medium text-gray-500 hover:text-gray-600 md:flex transition">
+                    <a href="#">Home</a>
+                    <a href="#">Technologies</a>
+                    <a href="#">Projects</a>
+                    <a href="#">About</a>
+                    <a href="#">Contact</a>
+                </div>
+                {/* Mobile menu */}
                 {menuOpen && (
-                    <div className="absolute left-0 top-full w-full border-b bg-white px-5 py-5 shadow md md:hidden">
+                    <div className="absolute left-0 top-full w-full border-b bg-white px-5 py-5 shadow">
                         <div className="flex flex-col gap-5 text-sm font-medium text-gray-500 hover:text-gray-600">
                             <a href="#" onClick={()=>setMenuOpen(false)}>Home</a>
                             <a href="#" onClick={()=>setMenuOpen(false)}>Technologies</a>
